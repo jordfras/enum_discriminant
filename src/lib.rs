c75e7d60dbf09ca1443e193d53e24d1bb560bf13
Thomas Johannesson <jordfras@gmail.com>
2025-05-09 20:17:17 +0200

    Prepare release 1.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![doc = include_str!("../README.md")]
 
-extern crate enum_discriminant_macro;
+extern crate enum_discriminant_macros;
 extern crate enum_discriminant_traits;
 
-pub use enum_discriminant_macro::*;
+pub use enum_discriminant_macros::*;
 pub use enum_discriminant_traits::FromDiscriminant;
 pub use enum_discriminant_traits::IntoDiscriminant;
